01086b445cbcfff3e875157e293e9877d19b48fd
Lucas-hrl <lucas.herchuel@uha.fr>
2025-09-16 11:47:26 +0200

    v3

--- TP1/rectangle.py
+++ TP1/rectangle.py
@@ -1,4 +1,7 @@
 from point import Point
 
 class Rectangle:
-    def __init__(self,pointBasGauche:Point,longueur:float,hauteur:float):
+    def __init__(self,pointBasGauche:Point = Point(0,0),longueur:float = 1,hauteur:float = 1):
+        self.__pointBasGauche = pointBasGauche
+        self.__longueur = longueur
+        self.__hauteur = hauteur
