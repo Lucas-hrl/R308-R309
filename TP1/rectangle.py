b7573bd771c5fc83c91f142ace29473edab71a98
Lucas-hrl <lucas.herchuel@uha.fr>
2025-09-17 10:34:08 +0200

    v4

--- TP1/rectangle.py
+++ TP1/rectangle.py
@@ -5,3 +5,36 @@ class Rectangle:
         self.__pointBasGauche = pointBasGauche
         self.__longueur = longueur
         self.__hauteur = hauteur
+
+    @property
+    def pointBasGauche(self) -> Point:
+        return self.__pointBasGauche
+
+    @pointBasGauche.setter
+    def pointBasGauche(self, pointBasGauche: Point):
+        self.__pointBasGauche = pointBasGauche
+
+    @property
+    def longueur(self) -> float:
+        return self.__longueur
+
+    @longueur.setter
+    def longueur(self, longueur: float):
+        self.__longueur = longueur
+
+    @property
+    def hauteur(self) -> float:
+        return self.__hauteur
+
+    @hauteur.setter
+    def hauteur(self, hauteur: float):
+        self.__hauteur = hauteur
+
+    def __str__(self):
+        return (f"Le rectangle a un point bas gauche a {self.__pointBasGauche}, "
+                f"une longueur de {self.__longueur} cm et une hauteur de {self.__hauteur} cm")
+
+
+if __name__ == "__main__":
+    rect1 = Rectangle()
+    print(rect1)
